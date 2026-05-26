4c99a20bd866a03cc64c620ca65bc934a0fdf895
Briander <pochitobriander@gmail.com>
2026-05-26 16:36:04 -0500

    clase EmpleadoServicio

--- src/servicio/EmpleadoServicio.java
+++ src/servicio/EmpleadoServicio.java
@@ -1,4 +1,64 @@
 package servicio;
 
+import modelo.*;
+import java.util.ArrayList;
+
+
 public class EmpleadoServicio {
+
+    private ArrayList<Empleado> empleados = new ArrayList<>();
+
+    public boolean existeCedula(String cedula){
+        for(Empleado e : empleados){
+            if (e.getCedula().equals(cedula)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void agregar(Empleado empleado){
+        empleados.add(empleado);
+    }
+
+    public void mostrarTodos(){
+        if (empleados.isEmpty()) {
+            System.out.println("No existen registros.");
+            return;
+        }
+
+        for (Empleado e : empleados) {
+            e.mostrarInformacion();
+            System.out.println("------------------");
+        }
+    }
+
+    public Empleado buscar(String cedula){
+        for(Empleado e : empleados){
+            if(e.getCedula().equals(cedula)){
+                return e;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean eliminar(String cedula) {
+
+        Empleado e = buscar(cedula);
+
+        if (e != null) {
+            empleados.remove(e);
+            return true;
+        }
+
+        return false;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    
 }
